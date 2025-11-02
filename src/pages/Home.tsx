51b69b46142c55ae08c2880cf01babc085b4b17a
Tyler <wjhyg01@gmail.com>
2025-11-01 21:49:59 -0700

    fix: rename ads-right to sponsor-rail to bypass ad blocker interference

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1442,7 +1442,7 @@ export default function Home() {
         </section>
 
         {/* 右侧广告栏 */}
-        <aside id="ads-right" className="w-[300px] shrink-0 h-full overflow-y-auto space-y-4" style={{ flex: '0 0 300px' }}>
+        <aside id="sponsor-rail" data-slot="right-rail" className="w-[300px] shrink-0 h-full overflow-y-auto space-y-4" style={{ flex: '0 0 300px' }}>
           <div className="h-[220px] rounded-xl bg-black/40 border border-white/10 flex items-center justify-center text-white/60">Right Rail</div>
           <div className="h-[220px] rounded-xl bg-black/40 border border-white/10 flex items-center justify-center text-white/60">Right Rail</div>
           <div className="h-[220px] rounded-xl bg-black/40 border border-white/10 flex items-center justify-center text-white/60">Right Rail</div>
